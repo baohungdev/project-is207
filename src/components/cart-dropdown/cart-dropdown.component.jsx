54e8bf2558092d0d9708e032a7cabaa5c469309c
Duong Bao Hung <baohungdev@icloud.com>
2020-06-23 16:28:28 +0700

    Implemented addItemtoCart and display the quantity to the CartIcon

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
 import CustomButton from "../custom-button/custom-button.component";
-
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
-const CartDropdown = () => {
+import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selector";
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>Go to checkout</CustomButton>
     </div>
   );
 };
 
-export default CartDropdown;
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -22,9 +22,7 @@ const Header = ({ currentUser, hidden }) => {
         </Link>
         {currentUser ? (
           <div>
-            <div className="option">
-              {currentUser.displayName.toUpperCase()}
-            </div>
+            <div className="option">{currentUser.displayName}</div>
             <div className="option" onClick={() => auth.signOut()}>
               SIGN OUT
             </div>
